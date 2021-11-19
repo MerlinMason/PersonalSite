d9680257a10d90cfb80f8bcefe7b09f630680e7a
Merlin Mason <info@merlinmason.co.uk>
2021-11-19 17:31:30 +0000

    Move styles to document to ensure overscroll background color

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 
-const Layout: React.FC = ({ children }) => (
-    <div className="bg-gradient-radial from-[#060636] via-[#201d4e] to-[#330a36] text-white text-xl selection:text-purple-600 selection:bg-gray-900">
-        {children}
-    </div>
-);
+const Layout: React.FC = ({ children }) => <main className="py-20">{children}</main>;
 
 export default Layout;
