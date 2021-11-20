be3dbf509e2c3b22f410c6f9e94de5367abf793f
Merlin Mason <info@merlinmason.co.uk>
2021-11-20 23:00:30 +0000

    Add hero responsive fontsize

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
             backgroundImage: {
                 "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
             },
+            fontSize: {
+                hero: ["clamp(2rem, -0.5000rem + 6.6667vw, 6rem)", { lineHeight: 1 }],
+            },
         },
     },
     plugins: [],
