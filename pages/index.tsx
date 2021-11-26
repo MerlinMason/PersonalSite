9e277f21a5078d78b02000a105bb3245b51bd56f
Merlin Mason <info@merlinmason.co.uk>
2021-11-26 01:00:48 +0000

    Update homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import Image from "next/image";
+
+import ExternalLink from "../components/ExternalLink";
 
 const Home: React.FC = () => (
     <>
         <h1 className="font-bold text-hero mb-12 text-white/90">
-            Hi 👋 <br />
-            I&apos;m Merlin (he/him).
+            Hi 👋 I&apos;m Merlin,
+            <br /> a software engineer living & working in London, UK.
         </h1>
 
         <div className="space-y-5 text-white/70 font-light leading-relaxed mb-6 p-4 sm:p-6 bg-white/5 shadow-2xl rounded">
             <h2 className="font-semibold text-2xl mb-6 text-white/80">
-                I&apos;m a software engineer passionate about building the teams, processes and
-                software that create amazing digital products.
+                I&apos;m passionate about building the teams, processes and software that create
+                amazing digital products.
             </h2>
             <p>
                 I have a T-shaped skill set with a deep understanding of the frontend stack and a
@@ -22,29 +23,16 @@ const Home: React.FC = () => (
 
             <p>
                 Most recently (2014 - 2021), I co-founded and led engineering at{" "}
-                <a
+                <ExternalLink
                     href="https://mo.work"
-                    target="_blank"
-                    rel="noreferrer"
                     className="text-purple-400 hover:text-purple-300"
                 >
                     Mo
-                </a>
+                </ExternalLink>
                 , building a 12 strong engineering team and a product used by 100,000 users from
                 scratch. With a small team we created a product that would often win over industry
                 incumbents (Yammer, Reward Gateway, Perkbox etc) in sales processes.
             </p>
-        </div>
-
-        <div className="flex justify-center mb-6">
-            <a
-                href="https://github.com/MerlinMason"
-                target="_blank"
-                rel="noreferrer"
-                className="opacity-80 hover:opacity-100 transition-opacity duration-150"
-            >
-                <Image src="/github.png" width={32} height={32} alt="GitHub OctoCat logo" />
-            </a>
         </div>
     </>
 );
