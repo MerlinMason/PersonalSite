e115018b8075c716d6fc0f90769fed2510e2cf57
Merlin Mason <info@merlinmason.co.uk>
2022-10-04 00:50:09 +0100

    Generate OG Images locally

--- src/utils/getOpenGraphImages.js
+++ src/utils/getOpenGraphImages.js
@@ -1,33 +1,58 @@
-/* eslint-disable no-console */
-const fetch = require("node-fetch");
 const fs = require("fs");
+const puppeteer = require("puppeteer-core");
+const { readdir } = require("fs/promises");
 
-const getScreenshot = async () => {
-    // Where we'll store images
-    const dir = "./public/ogimages";
+const OUTPUT_PATH = "./public/ogimages"; // where the created screenshots will go
+const CHROME_PATH = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"; // Chrome on your local machine
+const POSTS_PATH = "./src/pages/blog"; // where your blog posts are
+const POSTS_FILE_EXTENSION = ".mdx"; // file type used for posts
+const OPEN_GRAPH_ROOT_URL = "http://localhost:3000/opengraph"; // where are we rendering the OGImages?
 
+// Creates a screenshot from a given URL
+const takeScreenshot = async (url) => {
     // Make sure the directory exists, if not create it
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir, { recursive: true });
+    if (!fs.existsSync(OUTPUT_PATH)) {
+        fs.mkdirSync(OUTPUT_PATH, { recursive: true });
     }
 
-    // Create the screenshot using 11ty's screenshot service
-    const response = await fetch(
-        `https://v1.screenshot.11ty.dev/${encodeURIComponent(process.env.VERCEL_URL)}/opengraph`
-    );
-
-    // Convert the response to a buffer
-    const arrayBuffer = await response.arrayBuffer();
-    const buffer = Buffer.from(arrayBuffer);
-
-    // Write the buffer to a file
-    fs.writeFile(`${dir}/image.png`, buffer, (err) => {
-        if (err) {
-            console.log("error", err);
-            return process.exit(1);
-        }
-        return true;
+    const browser = await puppeteer.launch({
+        product: "chrome",
+        args: [],
+        executablePath: CHROME_PATH,
+        headless: true,
     });
+
+    const page = await browser.newPage();
+
+    await page.setViewport({ width: 1200, height: 630 });
+    await page.goto(url);
+    await page.screenshot({
+        path: `${OUTPUT_PATH}/${url.replace(OPEN_GRAPH_ROOT_URL, "")}.png`,
+    });
+    await browser.close();
 };
 
-getScreenshot();
+// Gets all .mdx files in the blog directory
+const getBlogPostFileNames = async () => {
+    const filenames = await readdir(POSTS_PATH);
+
+    return filenames.filter((filename) => filename.endsWith(POSTS_FILE_EXTENSION));
+};
+
+// Converts filenames into URLs for the screenshot
+const getOpenGraphUrls = (filesNames) =>
+    filesNames.map(
+        (fileName) => `${OPEN_GRAPH_ROOT_URL}/${fileName.replace(POSTS_FILE_EXTENSION, "")}`
+    );
+
+// Main function
+(async () => {
+    const urls = getOpenGraphUrls(await getBlogPostFileNames());
+
+    urls.forEach(async (url) => {
+        await takeScreenshot(url);
+    });
+
+    // eslint-disable-next-line no-console
+    console.log(`📸 Successfully created ${urls.length} screenshots`);
+})();
