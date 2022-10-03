dd602892f3bfc38e872e20faeb9e97a3ae610496
Merlin Mason <info@merlinmason.co.uk>
2022-10-03 22:41:55 +0100

    WIP - basic test to determine if image generation will run in Vercel CI

---
+++ src/utils/getOpenGraphImages.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-console */
+const fetch = require("node-fetch");
+const fs = require("fs");
+
+const getScreenshot = async () => {
+    // Where we'll store images
+    const dir = "./dist/ogimages";
+
+    // Make sure the directory exists, if not create it
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+    }
+
+    // Create the screenshot using 11ty's screenshot service
+    const response = await fetch(
+        `https://v1.screenshot.11ty.dev/${encodeURIComponent(
+            "https://merlinmason.co.uk"
+        )}/opengraph`
+    );
+
+    // Convert the response to a buffer
+    const arrayBuffer = await response.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    // Write the buffer to a file
+    fs.writeFile(`${dir}/image.png`, buffer, (err) => {
+        if (err) {
+            console.log("error", err);
+            return process.exit(1);
+        }
+        return true;
+    });
+};
+
+getScreenshot();
