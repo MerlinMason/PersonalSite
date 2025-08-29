5dbb91adf0077c855faaeda6057df8891eaedf4e
Merlin Mason <info@merlinmason.co.uk>
2025-08-29 17:57:31 +0100

    remove trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://merlinmason.co.uk/",
+    site: "https://merlinmason.co.uk",
     integrations: [
         mdx(), 
         tailwind(), 
