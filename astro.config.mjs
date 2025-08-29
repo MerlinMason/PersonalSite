7e6985d643dae061b418887ef6089dee9910191f
Merlin Mason <info@merlinmason.co.uk>
2025-08-29 18:22:20 +0100

    Remove trailing slashes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
     site: "https://merlinmason.co.uk",
+    trailingSlash: 'never',
     integrations: [
         mdx(),
         tailwind(),
